c61b4dd092f6e7a86981294ad1083b80cc786adb
Himanshu Kawale <himanshukawale0@gmail.com>
2021-08-03 21:21:53 +0530

    settings.py to deploy

--- quizengine/settings.py
+++ quizengine/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'quizengineproject.herokuapp.com']
+ALLOWED_HOSTS = ['quizengineproject.herokuapp.com']
 
 PROJECT_ROOT = os.path.dirname("quizengine")
 
